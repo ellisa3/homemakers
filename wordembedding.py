d489ec5735d6817d87f036e267df84714dc1200a
Aldo Polanco <aldopolanco@Aldos-MacBook-Pro.local>
2022-10-10 16:13:25 -0500

    Added some details

--- wordembedding.py
+++ wordembedding.py
@@ -12,16 +12,23 @@ class wordEmbedding:
         glove2word2vec(glove_file, word2vec_glove_file)
         self.model = KeyedVectors.load_word2vec_format(word2vec_glove_file, binary=False)
     
-    def generateSimilar(self, sampleWord):
+    def generateOneSimilar(self, sampleWord):
         result = self.model.similar_by_word(sampleWord)
         most_similar_key, similarity = result[0]  # look at the first match
         return (most_similar_key, similarity)
+
+    
     
 
 def main():
     fp = '/Users/aldopolanco/homemakers/w2v_gnews_small.txt'
     we = wordEmbedding(fp)
+    #Generate closest word and its similarities
     print(we.generateSimilar("man"))
+    #Access a token's vector
+    print(we.model["man"]) 
+    #Access list of tokens in our word embedding
+    #print(we.model.index_to_key)
 
         
 main()
