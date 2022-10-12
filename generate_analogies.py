ef66f6822af46d3bf84f0b74cc17b2422a31c53a
ellsa3 <ellisa3@carleton.edu>
2022-10-11 21:16:14 -0500

    implementing pseudocode

--- generate_analogies.py
+++ generate_analogies.py
@@ -7,17 +7,27 @@
 #pseudocode
 
 # find cosine similarity between she, he
-# she_he_similarity = most_similar(positive = [she, topn=10) //solid guess that given she, he will be within top 10 most similar words
+# she_he_similarity = most_similar(positive = [she], topn=10)      //solid guess that given she, he will be within top 10 most similar words
 # for every word in x
     # cosine_similarities = []
     # cosine_similarities = most_similar(positive = [x], topn=None) //returns all the words in the vocab & their cosine similarity relative to x
 
     # curr_difference = 0
-    # best_difference = 10
+    # min_difference = 10
+    # min_theta = 0                                             //not sure what type this boi is
     # for theta in cosine_similarities
-        # curr_difference = abs_val(she_he_similarity - theta)
-        # if curr_difference < best_difference
-            # best_difference = curr_difference
+        # curr_difference = abs_val(she_he_similarity - theta)  //want to find how similar the distance values are to each other
+        # if curr_difference < min_difference
+            # min_difference = curr_difference                  
+            # min_theta = theta                                 //keep track of the [word, degree] that minimizes that difference
 
-    #return best_difference
+    #return min_theta
+
+from random import sample
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.test.utils import datapath, get_tmpfile
+
+    # return cosine similarity between she, he
+
         
