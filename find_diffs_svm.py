d38301baf8ef584c12ea59d1d7525f308c0c368a
Aishwarya <varmaa@carleton.edu>
2022-11-04 04:00:14 +0000

    wip version of their code

---
+++ find_diffs_svm.py
@@ -0,0 +1,43 @@
+# find diffs between their gender neutral words and our gender neutral words
+
+# load gender_specific_full and fins which ones overlap
+
+import json
+
+class FindDiffs:
+  def __init__(self):
+    self.missing_words = set()
+    self.commonalities = set()
+    self.false_words = set()
+
+  def find_diffs(self):
+    gendered_theirs = set()
+    with open('./data/gender_specific_seed_words.json', "r") as f:
+        gendered_theirs = json.load(f)
+
+    print("length of gendered_theirs", len(gendered_theirs))
+
+    gendered_ours = set(line.strip() for line in open('./data/gender_specific_predict.txt'))
+    print("length of gendered_ours", len(gendered_ours))
+    # print(gendered_ours)
+    # print("\n")
+    # print(gendered_theirs)
+
+    for word in gendered_theirs:
+      if word not in gendered_ours:
+        self.missing_words.add(word)
+      else:
+        self.commonalities.add(word)
+
+    for word in gendered_ours:
+      if word not in gendered_theirs:
+        self.false_words.add(word)
+
+def main():
+  fd = FindDiffs()
+  fd.find_diffs()
+  print("Number of words missing in our set: ", len(fd.missing_words))
+  print("Number of commonalities: ", len(fd.commonalities))
+  print("Number of false words in our set: ", len(fd.false_words))
+
+main()
--- linearsvm2.py
+++ linearsvm2.py
@@ -1,7 +1,8 @@
 from __future__ import print_function, division
 import sys
 import argparse
-from we import *
+import wordembedding
+import numpy as np
 from sklearn.svm import LinearSVC
 import json
 if sys.version_info[0] < 3:
@@ -15,39 +16,39 @@ Tolga Bolukbasi, Kai-Wei Chang, James Zou, Venkatesh Saligrama, and Adam Kalai
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("embedding_filename", help="The name of the embedding")
 parser.add_argument("NUM_TRAINING", type=int)
-parser.add_argument("GENDER_SPECIFIC_SEED_WORDS")
-parser.add_argument("outfile")
 
 args = parser.parse_args()
 
-embedding_filename = args.embedding_filename
 NUM_TRAINING = args.NUM_TRAINING
-GENDER_SPECIFIC_SEED_WORDS = args.GENDER_SPECIFIC_SEED_WORDS
-OUTFILE = args.outfile
+GENDER_SPECIFIC_SEED_WORDS = "./data/gender_specific_seed_words.json"
+OUTFILE = "gender_specific_predict2.txt"
 
 with open(GENDER_SPECIFIC_SEED_WORDS, "r") as f:
     gender_seed = json.load(f)
 
 print("Loading embedding...")
-E = WordEmbedding(embedding_filename)
+E = wordembedding.WordEmbedding(isLinearSVM = True)
 
-print("Embedding has {} words.".format(len(E.words)))
+print("Embedding has {} words.".format(len(E.model.index_to_key)))
 print("{} seed words from '{}' out of which {} are in the embedding.".format(
     len(gender_seed),
     GENDER_SPECIFIC_SEED_WORDS,
-    len([w for w in gender_seed if w in E.words]))
+    len([w for w in gender_seed if w in E.model.index_to_key]))
 )
 
-gender_seed = set(w for i, w in enumerate(E.words) if w in gender_seed or (w.lower() in gender_seed and i<NUM_TRAINING))
-labeled_train = [(i, 1 if w in gender_seed else 0) for i, w in enumerate(E.words) if (i<NUM_TRAINING or w in gender_seed)]
+gender_seed = set(w for i, w in enumerate(E.model.index_to_key) if w in gender_seed or (w.lower() in gender_seed and i<NUM_TRAINING))
+labeled_train = [(i, 1 if w in gender_seed else 0) for i, w in enumerate(E.model.index_to_key) if (i<NUM_TRAINING or w in gender_seed)]
 train_indices, train_labels = zip(*labeled_train)
 y = np.array(train_labels)
-X = np.array([E.vecs[i] for i in train_indices])
+# what is E.vecs[i] ? for i in train_indices? what is train_indices?
+# how do i go through the list of vectors
+vecs = [E.model[w] for w in E.model.index_to_key]
+X = np.array([vecs[i] for i in train_indices])
 C = 1.0
 clf = LinearSVC(C=C, tol=0.0001)
 clf.fit(X, y)
+# what are the weights? i didn't do this part in my code
 weights = (0.5 / (sum(y)) * y + 0.5 / (sum(1 - y)) * (1 - y))
 weights = 1.0 / len(y)
 score = sum((clf.predict(X) == y) * weights)
@@ -57,9 +58,9 @@ pred = clf.coef_[0].dot(X.T)
 direction = clf.coef_[0]
 intercept = clf.intercept_
 
-is_gender_specific = (E.vecs.dot(clf.coef_.T) > -clf.intercept_)
+is_gender_specific = (vecs.dot(clf.coef_.T) > -clf.intercept_)
 
-full_gender_specific = list(set([w for label, w in zip(is_gender_specific, E.words)
+full_gender_specific = list(set([w for label, w in zip(is_gender_specific, E.model.index_to_key)
                             if label]).union(gender_seed))
 full_gender_specific.sort(key=lambda w: E.index[w])
 
