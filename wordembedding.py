55c1f96ae29ed8a6cd9d01014c2c629672320411
Aldo Polanco <polancoa@carleton.com>
2022-11-19 12:15:43 -0600

    Cleaned we.py

--- wordembedding.py
+++ wordembedding.py
@@ -32,40 +32,6 @@ class WordEmbedding:
             with open("./data/Scatterplot.json") as spfile:
                 self.scatterplot = json.load(spfile)
 
-    
-    def generateOneSimilar(self, sampleWord): #this function exists in keyedVectors, most_similar() (set param N to 1 to get most similar word) line 776 of documentation
-        result = self.model.similar_by_word(sampleWord)
-        most_similar_key, similarity = result[0]  # look at the first match
-        return (most_similar_key, similarity)
-    
-    def generateNSimilar(self, input_words, num_sim):
-        ## Generate a list of n similar words to a list of input words
-        def append_list(sim_words, words):
-            list_of_words = []
-            
-            for i in range(len(sim_words)): # Create tuple of words & similarity
-                
-                sim_words_list = list(sim_words[i])
-                sim_words_list.append(words)
-                sim_words_tuple = tuple(sim_words_list)
-                list_of_words.append(sim_words_tuple)
-
-            print("list of words", list_of_words)    
-            return list_of_words
-        
-        user_input = [word.strip() for word in input_words.split(',')]
-        result_words = []
-
-        for words in user_input:
-            sim_words = self.model.most_similar(words, topn = num_sim)
-            sim_words = append_list(sim_words, words)
-            result_words.extend(sim_words)
-
-        # most_similar_keys = [word[0] for word in result_words]
-        # similarity = [word[1] for word in result_words]
-        # similar_to = [word[2] for word in result_words]
-        return result_words
-
     def debias(self, gendered):
         #Find gender direction
         direction = self.findBiasDirection()
@@ -91,13 +57,6 @@ class WordEmbedding:
                 z = -z
             self.model[w1] = z * direction + y
             self.model[w2] = -z * direction + y
-            # mu = (self.model[w1] + self.model[w2])/2
-            # muproj = self.project(mu, direction)
-            # v = mu - muproj
-            # w1proj = self.project(w1, direction)
-            # w2proj = self.project(w2, direction)
-            # self.model[w1] = v + (np.sqrt((1 - np.linalg.norm(v)**2)) * (w1proj - muproj)/np.linalg.norm(w1proj - muproj))
-            # self.model[w2] = v + (np.sqrt((1 - np.linalg.norm(v)**2)) * (w2proj - muproj)/np.linalg.norm(w2proj - muproj))
 
         self.norm()
         return None
@@ -123,13 +82,11 @@ class WordEmbedding:
         toFit = []
         for w1, w2 in self.definition_pairs:
             if w1.lower() not in self.model and w1 not in self.model:
-                print("PROBLEM:", w1)
                 continue
             else:
                 if w1.lower() in self.model and w1 not in self.model:
                     w1 = w1.lower()
             if w2.lower() not in self.model and w2 not in self.model:
-                print("PROBLEM:", w2)
                 continue
             else:
                 if w2.lower() in self.model and w2 not in self.model:
@@ -148,7 +105,6 @@ class WordEmbedding:
         toFit = []
         for w1, w2 in definition_pairs:
             if w1 not in self.model.key_to_index or w2 not in self.model.key_to_index:
-                print('Word Not found:')
                 continue
             #Find average between two vector pairs such as (man, woman) or (he, she)
             average = (self.model[w1] + self.model[w2])/2
@@ -160,31 +116,3 @@ class WordEmbedding:
         pca = PCA(n_components=10)
         pca.fit(toFit)
         return pca
-
-def main():
-    we = WordEmbedding("content/homemakers/data/w2v_gnews_vsmall.txt")
-    # diff = we.normOne(we.model["handbag"] - we.model["she"])
-    # softball = we.normOne(we.normOne(we.model["he"]) + diff)
-    # print("diff:", we.model.similar_by_vector(diff, topn=3))
-    # print("should be softball:", we.model.similar_by_vector(softball, topn=3))
-    # print("woman:", we.model.distance("woman", "homemaker"))
-    # print("man:", we.model.distance("man", "homemaker"))
-
-    # specific = open("data/genderedPaper.json")
-    # specificwords = json.load(specific)
-    # #specificwords = [word.lower() for word in specificwords]
-    # we.debias(specificwords)
-    # print("NEUTRALIZED")
-    # diff = we.normOne(we.model["handbag"] - we.model["she"])
-    # softball = we.normOne(we.model["he"] + diff)
-    # print("diff:", we.model.similar_by_vector(diff, topn=3))
-    # print("should be softball:", we.model.similar_by_vector(softball, topn=3))
-    # print("woman:", we.model.distance("woman", "homemaker"))
-    # print("man:", we.model.distance("man", "homemaker"))
-
-
-
-    
-    print("Done")
-    
-#main()
